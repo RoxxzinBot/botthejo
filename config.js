7be5fd8fc15c4fa6c2319ce11b1b5e5fc80e3cb0
RoxxzinBot <122653486+RoxxzinBot@users.noreply.github.com>
2023-02-21 15:59:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,90 @@
+/**
+   * Create By FARIZ THE JO
+   * Base by Dika Ardnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+  zenz: 'https://zenzapis.xyz',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  dhnjing: 'https://dhnjing.xyz',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  ana: 'https://anabotofc.herokuapp.com/',
+  amel: 'https://melcanz.com',
+  hardianto: 'https://hardianto.xyz',
+  botstyle: 'https://botstyle-api.herokuapp.com',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  kanx: 'https://kannxapi.herokuapp.com/', 
+  violetics : 'https://violetics.pw',
+  ziy : 'https://ziy.herokuapp.com',
+  males : 'https://malesin.xyz',
+  pasha : 'https://api.nataganz.com'
+}
+
+// Apikey Website Api
+global.APIKeys = {
+  'https://zenzapis.xyz': 'Your Key',
+  'https://api.xteam.xyz': 'ebb6251cc00f9c63',
+  'https://anabotofc.herokuapp.com/': 'AnaBot',
+  'https://api.lolhuman.xyz': 'e54205a4ca2caa368cc067bb',// 8e66d0934cf741bfd2182c16
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://melcanz.com': 'melcantik',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://botstyle-api.herokuapp.com': 'Apikeymu',
+  'https://hardianto.xyz': 'hardianto',
+  'https://violetics.pw': 'beta',
+  'https://ziy.herokuapp.com' : 'xZiyy',
+  'https://api.nataganz.com' : 'Pasha'
+}
+
+
+global.ownername = 'PELER RIMEK'
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+// Other
+global.owner = ['6283803489747','6283803489747','6283803489747']
+global.premium = ['6283803489747']
+global.packname = ';V'
+global.author = 'F'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
